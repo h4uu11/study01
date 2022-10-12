6fcf3c235def6c74c22d982203c9b86b61ccc0ac
DESKTOP-A33RVNF\김동인 <ehddls960617@naver.com>
2022-10-12 14:33:29 +0900

    fix. eslint setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
+/* eslint-disable prettier/prettier */
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+    "plugin:vue/essential",
+    "airbnb-base",
+    "@vue/eslint-config-typescript",
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest"
+  },
+  rules: {
+    semi: 0, 
+    "import/no-unresolved": "off", 
+    "comma-dangle": "off", 
+    "no-new": 0, 
+    indent: ["error", 2],
+    "import/extensions": "off",
   }
 }
